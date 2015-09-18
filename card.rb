20af97a0f409fe340c7832c1903ba17f093694e7
AgentWang007 <jaybwang007@gmail.com>
2015-09-18 12:21:51 -0400

    add a card.rb and rspec

---
+++ card.rb
@@ -0,0 +1,19 @@
+class Card
+
+  def card(args = {})
+    @term = args[:term]
+    @def = args[:def]
+    @complete = args(:complete, false)
+  end
+
+  def mark_complete
+
+  end
+
+  def to.s
+
+  end
+
+end
+
+
