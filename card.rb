0acee9abf9ee27366d3b8730e321dc7d3962087a
AgentWang007 <jaybwang007@gmail.com>
2015-09-18 13:51:49 -0400

    add to_s code and guess, got correct? method

--- card.rb
+++ card.rb
@@ -1,23 +1,37 @@
 class Card
 
-  def card(args = {})
+  attr_reader :term, :def
+  attr_accessor :complete
+
+  def initialize(args = {})
     @term = args[:term]
     @def = args[:def]
-    @complete = fetch(:complete, false)
+    @complete = args.fetch(:complete, false)
   end
 
   def mark_complete
-
+    @complete = true
   end
 
-  def to.s
-
+  def to_s
+    "#{self.term}: #{self.def}"
   end
 
   def guess(string)
+    if term == string
+      mark_complete
+  end
+
+  def got_correct?
+
 
   end
 
 end
+
+# driver test
+
+test = Card.new({term: 'alias', def: 'To create a second name for the variable or method'})
+print test
 
 
