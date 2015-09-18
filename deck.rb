5307b84aea3dfaa19d7bb8bad0bd180028285536
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-09-18 12:36:50 -0400

    Implement basic instance variables and methods for the deck.

--- deck.rb
+++ deck.rb
@@ -1,27 +1,31 @@
 class Deck
+  attr_reader :name, :cards
+  attr_accessor: :card_idx
+
   def initialize(args = {})
     @name = args[:name] || "Flash Cards"
     @cards = args[:cards] || []
+    @card_idx = 0
   end
 
   def shuffle
+    cards.shuffle!
+  end
 
+  def current_card
+    cards[card_idx]
   end
 
   def next_card
-
+    card_idx += 1
   end
 
   def count_correct
-
+    cards.select{ |card| card.complete }.size
   end
 
-  def skip_card
-
-  end
-
-  def guess
-
+  def guess(attempt)
+    current_card.guess(attempt)
   end
 
 end
