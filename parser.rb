57b0b6a0322278e7781d60dd235c12c1dcc4e46f
Laura Evans <email": null>
2015-09-18 14:28:46 -0400

    add working parser

--- parser.rb
+++ parser.rb
@@ -6,13 +6,31 @@ module Parser
   def read(filepath)
     # binding.pry
     array = []
-    File.readlines(filepath, 'r').each do |line|
+    File.readlines(filepath).each do |line|
       array <<line
     end
-    puts array
+    array
   end
 
+  def cards(file)
+    deck = []
+    card = {}
+    lines_per_card = 3
 
-
+    read(file).each_with_index do |line, index|
+      if index % lines_per_card == 0
+        card[:definition] = line
+      elsif index % lines_per_card == 1
+        card[:term] = line
+      elsif index % lines_per_card == 2
+        deck << card
+        card = {}
+      end
+    end
+     deck
+  end
 
 end
+
+
+
