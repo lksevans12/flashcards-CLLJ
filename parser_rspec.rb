567ea3beb32bf4607efc657537405cfa87a7f5a8
Laura Evans <email": null>
2015-09-18 14:27:35 -0400

    add 2 more tests, both pass

--- parser_rspec.rb
+++ parser_rspec.rb
@@ -2,11 +2,21 @@ require_relative 'parser'
 
 describe Parser do
   let(:cards) { Parser.read('flashcard_samples.txt') }
-  let(:new_cards) { CardParser.make_cards('flashcard_samples.txt') }
+  let(:new_cards) { Parser.cards('flashcard_samples.txt') }
 
   describe '#parser' do
     it 'should return an Array' do
       expect(cards).to be_an_instance_of Array
+    end
+  end
+
+  describe '#cards' do
+    it 'should return an Array for the deck' do
+      expect(new_cards).to be_an_instance_of Array
+    end
+
+    it 'should have Hash objects for the cards' do
+      expect(new_cards.first).to be_an_instance_of Hash
     end
   end
 end
