240fb982ec87c7eeb456802b7d26915d742ae77b
Laurhens D <drhens@gmail.com>
2015-09-18 17:50:05 -0400

    update controller - cleaned up code

--- controller.rb
+++ controller.rb
@@ -11,48 +11,24 @@ class Controller
   def initialize(file)
     @file = file
     @view = View.new
-    # create array of hash of cards
     @cards = Parser.cards(file).map { |card_hash| Card.new(card_hash) }
-    # instantiate a deck populated w/ cards
     @deck = Deck.new({cards: cards})
   end
-    # retrieve hash from parser
-    # creating array of cards from hash
-    # create new deck w/ array
-    #run
 
   def run
     deck.shuffle!
     view.welcome
-    unless deck.finished?
+    while !deck.finished?
       play_turn
     end
     view.game_over
-    # view.question_prompt
-    # #display current card (definition)
-    # definition = deck.current_definition
-    # view.to_s(definition)
-
-    # view.answer_prompt
-    # #display: user "guess"
-    # #retrieve user input
-    # input = view.input
-    # unless input == "exit"
-    #check input: return T/F
-    # deck.guess_for_current_card(input)
-
-    # #flip_card if T
-    #logic here to run program?
   end
 
   def play_turn
     view.question_prompt
-    #display current card (definition)
     definition = deck.current_definition
     view.to_s(definition)
     view.answer_prompt
-    #display: user "guess"
-    #retrieve user input
     input = view.input
     if input == "exit"
     elsif input == "skip"
@@ -67,5 +43,4 @@ class Controller
   end
 end
 
-#calling #run to run program
 Controller.new('flashcard_samples.txt').run
