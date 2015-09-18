78c9c9c62e5f6e4d9d691d5f1eb5b0320cfb2548
Laurhens D <drhens@gmail.com>
2015-09-18 13:29:57 -0400

    update initial controller file

--- controller.rb
+++ controller.rb
@@ -1,5 +1,17 @@
-module Controller
-  module_function
+require_relative 'model'
+require_relative 'view'
 
+class Controller
+  attr_reader :model, :view
+  def initialize(params = {})
+    @model = params[:model]
+    @view = params[:view]
+  end
 
+  def run
+    #logic here to run program?
+  end
 end
+
+#calling #run to run program
+# Controller.new().run
