7339fff5e586c4318ebec5599ec6a2ea755e8774
Chanyoung Kim <chanyoung.j.kim@gmail.com>
2015-09-18 12:23:02 -0400

    Add skeleton methods

---
+++ deck.rb
@@ -0,0 +1,27 @@
+class Deck
+  def initialize(args = {})
+    @name = args[:name] || "Flash Cards"
+    @cards = args[:cards] || []
+  end
+
+  def shuffle
+
+  end
+
+  def next_card
+
+  end
+
+  def count_correct
+
+  end
+
+  def skip_card
+
+  end
+
+  def guess
+
+  end
+
+end
